894cfd90b846c37efcfda7e8db423128ccc427f1
Jussi Kinnunen <kinnunen.jussi@hotmail.com>
2019-10-19 01:04:03 +0300

    Implement multivariate walk-forward forecasting

--- noisedetector.py
+++ noisedetector.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.vector_ar.var_model import VAR
 
-class NoiseDetector:
+class NoiseDetectorUni:
     
     def __init__(self, data):
         self.model = AR(data).fit()
@@ -20,20 +21,45 @@ class NoiseDetector:
         squared_error = (actual - prediction) ** 2
         index = np.arange(order, len(data))
         frame = pd.DataFrame(
-                data=np.vstack((actual, prediction, squared_error)).T,
+                data=squared_error,
                 index=index,
-                columns=['actual', 'prediction', 'squared_error'])
+                columns=['squared_error'])
         frame.index.name = 'sample_index'
-        return frame
+        return (actual, prediction, frame)
+    
+class NoiseDetectorMulti:
+    
+    def __init__(self, data):
+        self.model = VAR(data).fit(ic='aic')
+        print('Model order: %s' % self.model.k_ar)
+
+    def walk_forward_forecast(self, data):
+        order = self.model.k_ar
+        features = self.model.neqs
+        if len(data) <= order:
+            raise ValueError('data length needs to be greater than model order')
+        prediction = np.array(self.model.forecast(data[:order], 1))
+        for i in range(1, len(data) - order):
+            end = i + order
+            prediction = np.vstack(
+                    (prediction, self.model.forecast(data[i:end], 1)))
+        actual = data[:-order]
+        squared_error = np.sum((actual - prediction) ** 2, axis=1) / features
+        index = np.arange(order, len(data))
+        frame = pd.DataFrame(
+                data=squared_error,
+                index=index,
+                columns=['squared_error'])
+        frame.index.name = 'sample_index'
+        return (prediction, actual, frame)
 
 def cull_error(frame, threshold=-1, percentage=-1, amount=-1):
     frame = frame.sort_values(by=['squared_error'], ascending=False)
     if threshold > -1:
-        frame = frame[frame['squared_error'] < threshold]
+        frame = frame[frame['squared_error'] > threshold]
     elif percentage > -1:
         frame = frame.head(int(len(frame) * (percentage / 100)))
     elif amount > -1:
         frame = frame.head(amount)
     frame = frame.sort_index()
     return frame
-
