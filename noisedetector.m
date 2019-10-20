62e04d637e62056d371c86daa9ad82110c08da0f
Jussi Kinnunen <kinnunen.jussi@hotmail.com>
2019-10-20 16:44:24 +0300

    Implement error listing and culling

--- noisedetector.m
+++ noisedetector.m
@@ -22,3 +22,15 @@ function e = walkForwardEstimate(model, input)
     end
     e = estimates;
 end
+
+function s = squaredErrorsOfWalkForward(actual, estimate)
+    order = numel(actual) - numel(estimate);
+    window = actual(order + 1:numel(actual));
+    errors = (window - estimate).^2;
+    errors = [errors (order+1:numel(actual)).'];
+    s = errors;
+end
+
+function s = cullErrors(errors, threshold)
+    s = errors(errors(:,1) >= threshold,:);
+end
