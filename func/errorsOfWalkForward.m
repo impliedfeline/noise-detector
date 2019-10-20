8eccaae365e1c64b016b63d75bad895cf44b2a4f
Jussi Kinnunen <kinnunen.jussi@hotmail.com>
2019-10-20 18:59:56 +0300

    Rename squared errors, use absolute value instead

--- func/errorsOfWalkForward.m
+++ func/errorsOfWalkForward.m
@@ -1,7 +1,7 @@
-function s = squaredErrorsOfWalkForward(actual, estimate)
+function s = errorsOfWalkForward(actual, estimate)
     order = numel(actual) - numel(estimate);
     window = actual(order + 1:numel(actual));
-    errors = (window - estimate).^2;
+    errors = abs(window - estimate);
     errors = [errors (order+1:numel(actual)).'];
     s = errors;
 end
