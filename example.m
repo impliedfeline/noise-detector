8177672ea6b478f0c71d78076fdaf44a16204ec1
impliedfeline <kinnunen.jussi@hotmail.com>
2019-10-22 04:45:11 +0300

    Update example.m

--- example.m
+++ example.m
@@ -1,26 +1,27 @@
-fs = 2048;
-dt = 1/fs;
-StopTime = 0.25;
-t = (0:dt:StopTime-dt)';
+% training data
+sampleRate = 2048;
+sampleTime = 1 / sampleRate;
+stopTime = 0.25;
+t = (0:sampleTime:stopTime-sampleTime)';
 F = 60;
 data = sin(2*pi*F*t);
 
-model = fitAr(data, dt, 50);
+% train the model
+model = fitAr(data, sampleTime, 1);
 estimate = walkForwardEstimate(model, data);
 order = numel(model.Report.Parameters.ParVector);
 % the estimate starts from the order + 1th element of the original array, since
 % the first order elements are only used to predict
 window = data(order + 1:numel(data));
 errors = errorsOfWalkForward(data, estimate);
-culled = cullErrors(errors, 0.035);
+culled = cullErrors(errors, 0.175);
 fig=figure; 
 hax=axes; 
 hold on
 plot(window);
 plot(estimate);
 for i = 1:size(culled,1)
-		sample = culled(i,2);
-		line([sample sample],get(hax,'YLim'),'Color',[1 0 0])
+	sample = culled(i,2);
+	line([sample sample],get(hax,'YLim'),'Color',[1 0 0])
 end
 hold off
-
